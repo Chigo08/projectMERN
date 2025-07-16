cd1e41085279a7eb53032d59cd1df7df303184da
Chigo08 <cmanusc3@gmail.com>
2025-07-16 14:50:50 +0800

    add totalQuantity to models

--- backend/src/models/Order.js
+++ backend/src/models/Order.js
@@ -24,6 +24,10 @@ const orderSchema = new mongoose.Schema(
       type: Number,
       min: 0,
     },
+    totalQuantity: {
+      type: Number,
+      min: 0,
+    },
     orderDate: {
       type: Date,
       default: Date.now,
@@ -33,13 +37,17 @@ const orderSchema = new mongoose.Schema(
 );
 
 orderSchema.pre("save", async function (next) {
-  let total = 0;
+  let priceTotal = 0;
+  let quantityTotal = 0;
   try {
     for (const item of this.menuOrdered) {
       const menu = await mongoose.model("Menu").findById(item.menu);
-      total += menu.price * item.quantity;
+      priceTotal += menu.price * item.quantity;
+      quantityTotal += item.quantity;
     }
-    this.totalPrice = total;
+    this.totalPrice = priceTotal;
+    this.totalQuantity = quantityTotal;
+
     next();
   } catch (error) {
     next(error);
